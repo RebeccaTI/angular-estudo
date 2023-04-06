59c3cb049899da20ddeae3e71a55342c8def80a1
Rebecca de Moura Coelho Rangel <47149015+RebeccaTI@users.noreply.github.com>
2023-04-06 19:41:06 -0300

    interpolacao de dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit{
+  name = "Rebecca";
+  age = 26;
+  job = "Programador"
+  hobbie = ["Jogar basquete","jogar futebol", "surfar"]
+  car = {
+    name: 'lamborghini',
+    year:2023
+
+  }
+
+  constructor(){  }
+  ngOnInit(): void {
+      
+  }
 
 }
